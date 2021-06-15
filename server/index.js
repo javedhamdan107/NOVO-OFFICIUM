7c1b32d8850c439b7136a57f21ad4d46d2ed115b
sanjaysunil34 <56962781+sanjaysunil34@users.noreply.github.com>
2021-06-15 17:36:08 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import Routes from './routes/index.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/user',Routes);
+
+app.get('/', (req,res) => {
+    res.send('Hello to Job Portal API');
+    console.log("hi");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
