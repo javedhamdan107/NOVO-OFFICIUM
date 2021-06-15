38bcd9d58285bb67685de6a4a30e70b6a51780cc
sanjaysunil34 <56962781+sanjaysunil34@users.noreply.github.com>
2021-06-15 17:33:05 +0530

    client

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { getUsers } from '../../actions';
+
+const HomeComponent = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getUsers());
+    },[dispatch]);
+
+    const users = useSelector((users) => users.home);
+    
+    return(
+        <div>
+            {users.map((user) => (
+                <p>{user.user_type}</p>
+            ))}
+        </div>
+    )
+}
+
+export default HomeComponent
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux';
+
+import home from '../components/Home/HomeReducer.js';
+
+export const reducers = combineReducers({ home });
