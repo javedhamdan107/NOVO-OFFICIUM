eb052ae082018ed6db05a9d8bf1b109e07b6ad76
sanjaysunil34 <56962781+sanjaysunil34@users.noreply.github.com>
2021-06-15 18:39:33 +0530

    deleted

--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers} from 'redux';
-
-import home from '../components/Home/HomeReducer.js';
-
-export const reducers = combineReducers({ home });
--- server/services/index.js
+++
@@ -1,8 +0,0 @@
-import Seeker from '../models/index.js';
-
-export const get_user = async () => {
-
-    const User = await Seeker.find();
-    return User;
-
-}
